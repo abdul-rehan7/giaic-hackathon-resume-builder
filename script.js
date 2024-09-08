d14c8c1058265560e100b6caba5322489770c1ad
rehanmirza76 <abdulrehan0317@gmail.com>
2024-09-08 17:43:41 +0500

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+//Object.defineProperty(exports, "__esModule", { value: true });///
 var html2pdf;
 function updateName() {
     // Get the user input values
