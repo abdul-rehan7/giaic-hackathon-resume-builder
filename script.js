46a557e91175be1e3f36b1f6c3c51a8d8b234551
rehanmirza76 <abdulrehan0317@gmail.com>
2024-11-07 13:32:40 +0500

    update

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function updateName() {
                 const opt = {
                     margin: 0,
                     filename: "Resume.pdf",
-                    image: { type: "jpeg", quality: 10 },
+                    image: { type: "jpeg", quality: 0.98 },
                     html2canvas: { scale: 2 },
                     jsPDF: { unit: "in", format:"letter", orientation: "portrait" },
                 };
